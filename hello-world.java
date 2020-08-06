22c546d31e7164bfdf889efe38246aaf90b4b9a3
jdromero88 <jdromerov88@gmail.com>
2020-08-05 23:45:35 -0400

    Create app Hello World

---
+++ hello-world.java
@@ -0,0 +1,8 @@
+// We create the class HelloWorld
+class HelloWorld{
+  // We call the main() method to start our application
+  public static void main(String args[]){
+    // WE print in line Hello World
+    System.out.println("Hello World!");
+  }
+}
